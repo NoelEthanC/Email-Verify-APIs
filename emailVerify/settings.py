4aedccb415ec8432efc3715f6ab9e424484369b4
Noel Ethan Chiwamba <mse16-nchiwamba@poly.ac.mw>
2022-12-20 15:24:55 +0200

    Update settings.py

--- emailVerify/settings.py
+++ emailVerify/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
